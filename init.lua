fe79fc20f53049acca6e262a39ae925301c57c70
Neon <neonfamxxi@gmail.com>
2024-10-08 20:18:06 -0700

    added todo comments, forced tabs to be 4 long

--- init.lua
+++ init.lua
@@ -157,6 +157,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -864,18 +867,32 @@ require('lazy').setup({
       signs = true, -- show icons in the signs column
       -- keywords recognized as todo comments
       keywords = {
+
+        --WHY = { icon = ' ', color = 'why', alt = { 'QUESTION', '???' } },
+
         FIX = {
           icon = ' ', -- icon used for the sign, and in search results
           color = 'error', -- can be a hex color, or a named color (see below)
           alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
           -- signs = false, -- configure signs for some keywords individually
         },
-        TODO = { icon = ' ', color = 'info' },
+        TODO = { icon = ' ', color = 'info', alt = { 'WHY', 'QUESTION' } },
         HACK = { icon = ' ', color = 'warning' },
-        WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX', 'WHY', 'QUESTION' } },
+        WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
         PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
         NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
         TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+      },
+
+      colors = {
+        why = { 'Identifier', '#5754FF' },
+
+        error = { 'DiagnosticError', 'ErrorMsg', '#DC2626' },
+        warning = { 'DiagnosticWarn', 'WarningMsg', '#FBBF24' },
+        info = { 'DiagnosticInfo', '#2563EB' },
+        hint = { 'DiagnosticHint', '#10B981' },
+        default = { 'Identifier', '#7C3AED' },
+        test = { 'Identifier', '#FF00FF' },
       },
     },
   },
